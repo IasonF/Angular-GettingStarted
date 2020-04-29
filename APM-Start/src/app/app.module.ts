931213869da2a11168550aa4db2a996f225f17bd
Iason Filippopoulos <iason.filippopoulos@objectivepartners.com>
2020-04-29 22:31:51 +0200

    Modularize it

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -2,39 +2,21 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {ProductListComponent} from "./products/product-list.component";
-import {FormsModule} from "@angular/forms";
-import {ConvertToSpacesPipe} from "./shared/convert-to-spaces.pipe";
-import {StarComponent} from "./shared/star.component";
 import {HttpClientModule} from "@angular/common/http";
-import {ProductDetailComponent} from './products/product-detail.component';
-import {RouterModule} from "@angular/router";
 import {WelcomeComponent} from "./home/welcome.component";
-import {ProductDetailGuard} from "./products/product-detail.guard";
+import {ProductModule} from './products/product.module';
+import {AppRoutingModule} from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    ProductDetailComponent
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
-    RouterModule.forRoot([
-      {path: 'products', component: ProductListComponent},
-      {
-        path: 'products/:id',
-        canActivate: [ProductDetailGuard],
-        component: ProductDetailComponent
-      },
-      {path: 'welcome', component: WelcomeComponent},
-      {path: '', redirectTo: 'welcome', pathMatch: 'full'},
-      // {path: '**', component: PageNotFoundComponent}
-    ])
+    ProductModule,
+    AppRoutingModule
   ],
   bootstrap: [AppComponent]
 })
